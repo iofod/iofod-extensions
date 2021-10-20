ff2aa87ea8587c2787299c8b4f5724c1572808c6
qkorbit <qkorbit@live.com>
2021-10-20 15:57:53 +0800

    dev: add dev-server

---
+++ demo/dev-server/server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var cors = require('cors')
+var fs = require('fs')
+var app = express()
+var port = 3000
+
+app.get('/:path', cors(), function (req, res) {
+  var path = __dirname + '/src/' + req.params.path
+
+  try {
+    if (fs.statSync(path).isFile()) {
+      res.sendFile(path)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (e) {
+    res.sendStatus(404)
+  }
+})
+
+app.listen(port, function () {
+  console.log('listening on port ' + port)
+})
---
+++ demo/dev-server/src/index.js
@@ -0,0 +1,15 @@
+// your code
+var $
+
+export async function main() {
+  $ = await PLUS.init({
+    state: {
+    },
+    persisted: false,
+    version: '1.0.0'
+  })
+
+  await PLUS.render([
+  ])
+}
+
